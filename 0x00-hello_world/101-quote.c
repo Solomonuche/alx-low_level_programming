492885e9038eb5afedc41405ec2ece1867b700ac
solomonuche <solomonuche@gmail.com>
2023-06-15 18:47:46 -0700

    hot fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,15 @@
 #include <unistd.h>
 #include <string.h>
+/**
+ * main - Entry point
+ * standard error function
+ *
+ * Return: (1)
+ */
 int main(void)
 {
 	char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(2, n, strlen(n));
 	return (1);
 }
