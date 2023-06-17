14d12e130c9f1a54b54d1a60d9d6b4f034bcc7c3
solomonuche <solomonuche@gmail.com>
2023-06-16 18:36:28 -0700

    hot fix

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * a program that prints single digit of base 10
+ * 
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int num;
+
+	num = 10;
+
+	while (num < 10)
+	{
+	printf("%d", num);
+	num++;
+	}
+	printf("\n");
+
+	return (0);
+}
