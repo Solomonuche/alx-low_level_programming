02d6a4123351bbba6ed2718a8567574dcb5b0556
solomonuche <solomonuche@gmail.com>
2023-06-20 19:30:35 -0700

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - a funxtion that prints natural numbers below 1024 that are
+ * multiplies of 5 or 3
+ *
+ * Return: Always (0) success
+ */
+int main(void)
+{
+	int i, sum;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += a;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
