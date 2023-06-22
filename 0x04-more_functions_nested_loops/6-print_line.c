a0556fe12ede8b543bc074bcaf5633527961fa11
solomonuche <solomonuche@gmail.com>
2023-06-22 10:28:09 -0700

    draw a line code

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_line - a function that draws a straight line in the terminal
+ * @n: number of times 
+ * followed by a new line
+ *
+ * Return: (0) success
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+/*	else if (n == 0)
+	{
+		_putchar('\n');
+	}
+*/
+	else
+	{
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
