01e3037c255586e8311c72bfed238ea43c889e6d
solomonuche <solomonuche@gmail.com>
2023-06-29 13:25:29 -0700

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,9 +18,10 @@ char *cap_string(char *str)
 
 	for (i = 1; str[i] != '\0'; i++)
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' ||
-				str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-				str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+			str[i] == ',' || str[i] == ';' ||
+			str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+			str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
 		{
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 			{
