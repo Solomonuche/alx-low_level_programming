c41cce4314096471fd53d5ae1fe20e9c81da8c1b
solomonuche <solomonuche@gmail.com>
2023-07-10 12:46:28 -0700

    using malloc function to allocate memory to a duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - a function that that returns a pointer
+ * whihc contains a copy of a given string
+ * @str: string
+ *
+ * Return: (ptr)
+ */
+
+char *_strdup(char *str)
+{
+	int i, len = 0;
+	char *ptr;
+
+	if (str != 0)
+	{
+		for (i = 0; str[i] != '\0'; i++)
+			len++;
+	}
+
+	ptr = (char *) malloc((len + 1) * sizeof(char));
+
+	if (str != 0)
+	{
+		for (i = 0; i < len; i++)
+			ptr[i] = str[i];
+		ptr[len] = '\0';
+	}
+
+	return (ptr);
+}
