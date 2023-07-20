a896b53a441eb18eb39f5ee30b4841663ca48c8c
solomonuche <solomonuche@gmail.com>
2023-07-20 12:14:03 -0700

    a unction that prints string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+/**
+ * print_numbers - a function that prints numbers,
+ * followed by a new line
+ * @n: number of integer passed
+ * @separator: integer seperator
+ *
+ * Return: (nothing)
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+	va_list strings;
+
+	va_start(strings, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(strings, char *);
+		if (str != NULL)
+			printf("%s", str);
+		else
+			printf("(nil)");
+
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	va_end(strings);
+	printf("\n");
+}
