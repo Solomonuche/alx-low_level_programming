2a4df32b7371247efe3dacec3e217c3a23347556
solomonuche <solomonuche@gmail.com>
2023-07-27 17:06:59 -0700

    a function that executes before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_b4_main - a function that executes automatically before main functio
+ *
+ * Return: (nothing)
+ */
+
+void print_b4_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
