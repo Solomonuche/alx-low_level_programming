6d887cc3a63d037227bdb508489a77c156d97589
solomonuche <solomonuche@gmail.com>
2023-07-27 17:28:59 -0700

    advanced task 1

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,8 +6,8 @@
  * Return: (nothing)
  */
 
-void print_b4_main(void)
+void print_auto(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
