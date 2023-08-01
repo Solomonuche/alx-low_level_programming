aca60946c61c205096d18e096522fe5a5acefd49
solomonuche <solomonuche@gmail.com>
2023-08-01 14:58:50 -0700

    delete a node from a list

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * delete_nodeint_at_index - a function that deletes
+ * a node at index of a linked list
+ * @head: linked list
+ * @idx: nth node
+ * @n: struct member
+ *
+ * Return: (nth node)
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	struct listint_s *temp = *head, *curentnode, *nxtnode;
+
+	if (head == NULL)
+		return (-1);
+
+	while (i < index - 1 && temp != NULL)
+	{
+		temp = temp->next;
+		i++;
+	}
+	if (temp == NULL && index != 0)
+	{
+		return (-1);
+	}
+
+
+	if (index == 0)
+	{
+		free(*head);
+	}
+	else
+	{
+		curentnode = temp->next;
+		nxtnode = curentnode->next;
+		temp->next = nxtnode;
+		free(curentnode);
+	}
+
+	return (1);
+}
