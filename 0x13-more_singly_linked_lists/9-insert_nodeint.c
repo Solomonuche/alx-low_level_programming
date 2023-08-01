a2450a3b5119a068ae8b16dd453d2bfaf4d8a583
solomonuche <solomonuche@gmail.com>
2023-08-01 14:29:03 -0700

    add a new node at a given postion

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 		i++;
 	}
-	if (i < idx - 1 && idx != 0)
+	if (temp == NULL && idx != 0)
 	{
 		return (NULL);
 	}
