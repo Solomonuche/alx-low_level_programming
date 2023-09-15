74d4e087eb3a69114e687b7fdd53d8d48c7bee89
solomonuche <solomonuche@gmail.com>
2023-09-14 21:23:57 -0700

    get list node at nth index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	}
 	else
 	{
-		while (i < index)
+		while (i < index && temp != NULL)
 		{
 			temp = temp->next;
 			i++;
