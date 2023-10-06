0e87c6786ea1234d371825ac0023cb0250a1f59e
Solomon <solomonuche@gmail.com>
2023-10-06 22:09:34 +0100

    a function that adds an element to the hash table.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,13 +12,34 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node = NULL;
+	hash_node_t *new_node = NULL, *current;
 	unsigned long int index = 0;
 
 	if (key == NULL || ht == NULL || value == NULL || !ht->size || !ht->array)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
+	current = ht->array[index];
+
+	/** Check if the key exists in the linked list at the given index*/
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			if (current->value == NULL)
+			{
+				return (0);
+			}
+			return (1);
+		}
+		current = current->next;
+	}
+
+	/** Key doesn't exist, create a new node and insert it at the beginning */
+
+
 	new_node = malloc(sizeof(*new_node));
 
 	if (new_node == NULL)
@@ -40,18 +61,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 
-	new_node->next = NULL;
-
-	/** adding node to the hash table*/
-	if (ht->array[index] == NULL)
-	{
-		ht->array[index] = new_node;
-	}
-	else
-	{
-		new_node->next = ht->array[index];
-		ht->array[index] = new_node;
-	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
 	return (1);
 
